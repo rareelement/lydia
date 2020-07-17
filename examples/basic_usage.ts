c0987a5a8aabcec7a26d0e1acf352e9243c01c21
Rare Element A <support@rareelements.ca>
2020-07-16 23:37:52 -0400

    Upgraded third party libs, added usage example

--- examples/basic_usage.ts
+++ examples/basic_usage.ts
@@ -1,9 +1,3 @@
-# lydia
-
-TypeScript RFC5545 Parser implementation
-
-### Usage Example
-```
 import axios from 'axios';
 import { Calendar, CalendarParser, Component } from '@rareelements/lydia';
 
@@ -54,18 +48,3 @@ async function loadCalendar(): Promise<(Calendar | undefined)> {
     }
 }
 )();
-
-```
-
-
-DISCLAIMER: This library, code samples and the documentation are provided "as is" without warranty of any kind, either express or implied. Use at your own risk.
-
-We make makes no warranty that
-
-- the software will meet your requirements
-- the software will be uninterrupted, timely, secure or error-free
-- the results that may be obtained from the use of the software will be effective, accurate or reliable
-- the quality of the software will meet your expectations
-- any errors in the software obtained from us will be corrected.
-
-We assume no responsibility for errors or omissions in the software or documentation.
